04a6d4fa5632180d9b127cc764b3d32eea8e4742
Paul Barnette <paul.barnette@gmail.com>
2017-12-16 22:30:52 +1100

    Added in the layout for the website

--- src/App.js
+++ src/App.js
@@ -18,14 +18,19 @@ class App extends Component {
 
   render () {
     return (
-      <div className='App'>
+      <div className='App container'>
         <header className='App-header'>
           <img src={logo} className='App-logo' alt='logo' />
           <h1 className='App-title'>Welcome to React</h1>
         </header>
-        <p className='App-intro'>
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div class='row'>
+          <div class='col-sm-6'>
+           col 1
+         </div>
+          <div class='col-sm-6'>
+           col 2
+         </div>
+        </div>
       </div>
     )
   }
---
+++ src/tags/Layout.js
@@ -0,0 +1,12 @@
+export default (
+  <div class='container'>
+    <div class='row'>
+      <div class='col-sm'>
+           col 1
+         </div>
+      <div class='col-sm'>
+           col 2
+         </div>
+    </div>
+  </div>
+)
