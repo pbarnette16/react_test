45f93d9835193556e62d4111515b32aa58a2bf2b
Paul Barnette <paul.barnette@gmail.com>
2017-12-29 14:55:39 +1100

    Update for test using enzyme

--- src/App.js
+++ src/App.js
@@ -1,39 +1,15 @@
-import React, { Component } from 'react'
+import React from 'react'
 import logo from './logo.svg'
 import './App.css'
-import {getData} from './services/service'
+import Results from './pages/Results/Results'
 
-class App extends Component {
-  constructor (props) {
-    super(props)
-  }
-
-  componentDidMount () {
-    console.log('componentWillMount')
-    getData()
-    .then(data => {
-      console.log('data %o', data)
-    })
-  }
-
-  render () {
-    return (
-      <div className='App container'>
-        <header className='App-header'>
-          <img src={logo} className='App-logo' alt='logo' />
-          <h1 className='App-title'>Welcome to React</h1>
-        </header>
-        <div class='row'>
-          <div class='col-sm-6'>
-           col 1
-         </div>
-          <div class='col-sm-6'>
-           col 2
-         </div>
-        </div>
-      </div>
+export default () => (
+  <div className='App container'>
+    <header className='App-header'>
+      <img src={logo} className='App-logo' alt='logo' />
+      <h1 className='App-title'>Welcome to React</h1>
+    </header>
+    <Results />
+  </div>
     )
-  }
-}
 
-export default App
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+import {shallow} from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+
+const Enzyme = require('enzyme')
+const EnzymeAdapter = require('enzyme-adapter-react-16')
+import 'jest-enzyme'
+
+// Setup enzyme's react adapter
+Enzyme.configure({ adapter: new EnzymeAdapter() })
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-});
+  // const div = document.createElement('div')
+  shallow(<App />)
+  // ReactDOM.render(<App />, div)
+})
