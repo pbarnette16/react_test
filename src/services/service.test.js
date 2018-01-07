fb0f39a4d5432ce064f644f18a4928bc237d465c
Paul Barnette <paul.barnette@gmail.com>
2018-01-08 00:42:58 +1100

    Create a mock service for testing the services

---
+++ src/services/service.test.js
@@ -0,0 +1,21 @@
+var axios = require('axios')
+var MockAdapter = require('axios-mock-adapter')
+
+// This sets the mock adapter on the default instance
+var mock = new MockAdapter(axios)
+
+// Mock any GET request to /users
+// arguments for reply are (status, data, headers)
+mock.onGet('/users').reply(200, {
+  users: [
+    { id: 1, name: 'John Smith' }
+  ]
+})
+
+test('Service loads mock data', () => {
+  axios.get('/users')
+  .then(function (response) {
+    console.log(response.data)
+  })
+})
+
